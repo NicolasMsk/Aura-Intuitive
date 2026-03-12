f5709704fee07d06965309180cb48148d6d1f59d
NicolasMsk <nicolas.musicki@gmail.com>
2026-03-12 11:07:34 +0100

    Ajout mail promo AURA5

--- src/server.ts
+++ src/server.ts
@@ -106,7 +106,7 @@ app.use(
   }),
 );
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public'), { redirect: false }));
 
 /* ═══════════════════════════════════════════════════════
    ROUTES
